d5186cc692d7379a9edbeebdd60421e197c6a7f7
Mohammad Karbalaee <muhammad.ksht@gmail.com>
2023-09-12 19:46:50 +0200

    Initial Commit

---
+++ vidplay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vidplay
+//
+//  Created by mohammadkarbalee on 12.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
